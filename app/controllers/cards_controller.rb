8c3d6128a096ad31cfde2d3f4cfc8755fafa24d9
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-20 18:10:04 -0600

    Basic layout for welcome page

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,11 @@
 class CardsController < ApplicationController
-  def index
+
+  def connection
+    Faraday.new('https://api.magicthegathering.io')
   end
+
+  def index
+    @standard_cards = JSON.parse(connection.get("/v1/cards?set=[dtk,ori,bfz,exp,ogw,soi]&pageSize=1000").body)
+  end
+
 end
