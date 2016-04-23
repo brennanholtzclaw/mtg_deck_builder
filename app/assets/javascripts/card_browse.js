e36fd7762dbd90e289a858050a203af40897d1d8
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-23 14:42:07 -0600

    abandoning ajax temporarily for more functionality

--- app/assets/javascripts/card_browse.js
+++ app/assets/javascripts/card_browse.js
@@ -1,27 +1,29 @@
-$(document).ready(function(){
-  $("body").on('click', "a.card", getCard)
-})
-
-function getCard(){
-  // event.preventDefault <-- probably not necessary here?
-  var card_name = $(this).data("name")
-  var card_set = $(this).data("set")
-  var card_image = $(this).data("image")
-
-  $.ajax({
-    url: "https://api.magicthegathering.io/v1/cards?name=" + card_name + "&set=" + card_set + ""
-    // params: {name: card_name, set: card_set}
-    method: GET
-    success: function(){
-      // $(".selected_card").html('')
-      $(".selected_card").html(
-        "<div class=card data-card-name="
-        + card_name
-        + " data-card-set="
-        + card_set
-        + ">"
-        + "<img src=card_image alt=card_name>"
-      )
-    }
-  })
-}
+// $(document).ready(function(){
+//   $("body").on('click', "a.card", getCard)
+// })
+//
+// function getCard(){
+//   // event.preventDefault <-- probably not necessary here?
+//   var card_name = $(this).data("name")
+//   var card_set = $(this).data("set")
+//   var card_image = $(this).data("image")
+//
+//   $.ajax({
+//     url: "https://api.magicthegathering.io/v1/cards?name=" + card_name + "&set=" + card_set + ""
+//     // params: {name: card_name, set: card_set}
+//     method: GET
+//     success: function(){
+//       // $(".selected_card").html('')
+//       $(".selected_card").html(
+//         "<div class=card data-card-name="
+//         debugger
+//         + card_name
+//         + " data-card-set="
+//         + card_set
+//         + ">"
+//         + "<img src=card_image alt=card_name>"
+//         + "</div>"
+//       )
+//     }
+//   })
+// }
