71aac355a90d0237eb9351132e97a9352ee1e5f8
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-23 19:13:14 -0600

    Add logic for creating new cards by multiverseID

--- app/controllers/sets_controller.rb
+++ app/controllers/sets_controller.rb
@@ -1,6 +1,7 @@
 class SetsController < ApplicationController
   def index
     @cards = MtgService.new.get_set_cards(params["set"])
+    @cards["cards"].each { |card| Card.from_controller(card)}
     # binding.pry
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,22 +1,42 @@
 class Card < ActiveRecord::Base
 
-  def self.from_service(card_info)
-    where(uid: card_info[:multiverseid]).first_or_create do |new_card|
-      new_card.multiverseid                = card_info."XXXXXX".multiverseid
-      new_card.name               = card_info."XXXXXX".extra.raw_info.name
-      new_card.manacost       = card_info."XXXXXX".extra.raw_info.manacost
-      new_card.colors              = card_info."XXXXXX".info.colors
-      new_card.cmc              = card_info."XXXXXX".info.cmc
-      new_card.type              = card_info."XXXXXX".info.type
-      new_card.rarity              = card_info."XXXXXX".info.rarity
-      new_card.set             = card_info."XXXXXX".info.set
-      new_card.text              = card_info."XXXXXX".info.text
-      new_card.flavor              = card_info."XXXXXX".info.flavor
-      new_card.power              = card_info."XXXXXX".info.power
-      new_card.toughness              = card_info."XXXXXX".info.toughness
-      new_card.imageurl              = card_info."XXXXXX".info.imageurl
-      new_card.layout              = card_info."XXXXXX".info.layout
-      new_card.names              = card_info."XXXXXX".info.names
+  # def self.from_service(card_info)
+  #   if card_info.colors.nil?
+  #     ColorlessCard.new
+  #   elsif card_info.colors.count > 1
+  #     GoldCard.new
+  #   elsif card_info.colors.first.downcase == "red"
+  #     RedCard.new
+  #   elsif card_info.colors.first.downcase == "blue"
+  #     BlueCard.new
+  #   elsif card_info.colors.first.downcase == "green"
+  #     GreenCard.new
+  #   elsif card_info.colors.first.downcase == "white"
+  #     WhiteCard.new
+  #   elsif card_info.colors.first.downcase == "black"
+  #     BlackCard.new
+  #   else
+  #     "hell I don't know..."
+  #   end
+  # end
+
+  def self.from_controller(card_info)
+    where(multiverseid: card_info[:multiverseid]).first_or_create do |new_card|
+      new_card.multiverseid   = card_info["multiverseid"]
+      new_card.name           = card_info["name"]
+      new_card.manacost       = card_info["manacost"]
+      new_card.colors         = card_info["colors"]
+      new_card.cmc            = card_info["cmc"]
+      new_card.type           = card_info["type"]
+      new_card.rarity         = card_info["rarity"]
+      new_card.set            = card_info["set"]
+      new_card.text           = card_info["text"]
+      new_card.flavor         = card_info["flavor"]
+      new_card.power          = card_info["power"]
+      new_card.toughness      = card_info["toughness"]
+      new_card.imageUrl       = card_info["imageurl"]
+      new_card.layout         = card_info["layout"]
+      new_card.names          = card_info["names"]
     end
   end
 
