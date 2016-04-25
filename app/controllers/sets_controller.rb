e90adcc41fc9c7b3ef2f07c548550be747806dec
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-25 14:18:12 -0600

    This was an aliased commit.

--- app/controllers/sets_controller.rb
+++ app/controllers/sets_controller.rb
@@ -3,6 +3,6 @@ class SetsController < ApplicationController
     response = MtgService.new.get_set_cards(params["set"])
     @cards = response["cards"].each { |card| Card.from_controller(card)}
     # https://api.magicthegathering.io/v1/cards?name=forest,island,swamp,plains,mountain need to get lands included
-    @deck = @deck.contents
+    @deck_cards = @deck.contents
   end
 end
