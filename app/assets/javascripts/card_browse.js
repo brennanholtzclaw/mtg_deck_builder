5f54f9ed449be7bf3aaff8156b99c5174b8f1da5
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-26 23:04:52 -0600

    Clobber assets - AJAX works again

--- app/assets/javascripts/card_browse.js
+++ app/assets/javascripts/card_browse.js
@@ -12,31 +12,87 @@ function getCard(){
     // make this url unique to the card clicked and have it return internal API data
     method: "GET",
     dataType: "json",
-    success: function(card){
-      $(".selected_card").html('');
-      $(card).function(){
-        // debugger
-        $(".selected_card").append(
-          "<div class=card data-card-name="
-          + card.name
-          + " data-card-multiverseid="
-          + card.multiverseid
-          + ">"
-          + "<h1>Selected Card</h1>"
-          + "<img src="
-          + card.imageUrl
-          + " alt="
-          + card.name
-          + ">"
-          + "<br>"
-          // + "<a class='btn btn-primary' role='button' method='POST' href='/deck_cards'> +1 your Deck </a>  "
-          + "<button class='btn btn-primary' formmethod='POST' href='/deck_cards'> +1 your Deck </button>  "
-          + "<a class='btn btn-warning' role='button' method='POST' href='/deck_cards'> +4 your Deck </a>"
-          + "</div>");
-      })
-    },
+    success: showCard,
     error: function(){
       alert("Something went wrong")
     }
   })
 }
+
+function showCard(card){
+  $(".selected-card").html('')
+  $(card).each(function(index, post){
+    // debugger
+    $(".selected-card")
+      .append("<div class=card data-card-name="
+      + card.name
+      + " data-card-multiverseid="
+      + card.multiverseid
+      + ">"
+      + "<h1>Selected Card</h1>"
+      + "<img src="
+      + card.imageUrl
+      + " alt="
+      + card.name
+      + ">"
+      + "<br>"
+      // + "<a class='btn btn-primary' role='button' method='POST' href='/deck_cards'> +1 your Deck </a>  "
+      + "<button class='btn btn-primary' formmethod='POST' href='/deck_cards'> +1 your Deck </button>  "
+      + "<a class='btn btn-warning' role='button' method='POST' href='/deck_cards'> +4 your Deck </a>"
+      + "</div>");
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//   $("body").on('click', "a.card", getCard)
+// })
+//
+// function getCard(){
+//   // event.preventDefault <-- probably not necessary here?
+//   var card_name = $(this).data("name")
+//   var card_multiverseid = $(this).data("multiverseid")
+//
+//   $.ajax({
+//     url: "http://127.0.0.1:3000/api/v1/cards/" + card_multiverseid,
+//     // make this url unique to the card clicked and have it return internal API data
+//     method: "GET",
+//     dataType: "json",
+//     success: function(card){
+//       $(".selected_card").html('')
+//       $(card).function(){
+//         // debugger
+//         $(".selected_card").html(
+//           "<div class=card data-card-name="
+//           + card.name
+//           + " data-card-multiverseid="
+//           + card.multiverseid
+//           + ">"
+//           + "<h1>Selected Card</h1>"
+//           + "<img src="
+//           + card.imageUrl
+//           + " alt="
+//           + card.name
+//           + ">"
+//           + "<br>"
+//           // + "<a class='btn btn-primary' role='button' method='POST' href='/deck_cards'> +1 your Deck </a>  "
+//           + "<button class='btn btn-primary' formmethod='POST' href='/deck_cards'> +1 your Deck </button>  "
+//           + "<a class='btn btn-warning' role='button' method='POST' href='/deck_cards'> +4 your Deck </a>"
+//           + "</div>");
+//       })
+//     },
+//     error: function(){
+//       alert("Something went wrong")
+//     }
+//   })
+// }
--- public/assets/application-7749d5a1799d7046d7625e11bb7d2d652186282f312456caefa895ee3b1daae5.js
+++ public/assets/application-7749d5a1799d7046d7625e11bb7d2d652186282f312456caefa895ee3b1daae5.js
@@ -11580,6 +11580,52 @@ return jQuery;
   }
 
 })( jQuery );
+$(document).ready(function(){
+  $("body").on('click', "a.card", getCard)
+})
+
+function getCard(){
+  // event.preventDefault <-- probably not necessary here?
+  var card_name = $(this).data("name")
+  var card_multiverseid = $(this).data("multiverseid")
+
+  $.ajax({
+    url: "http://127.0.0.1:3000/api/v1/cards/" + card_multiverseid,
+    // make this url unique to the card clicked and have it return internal API data
+    method: "GET",
+    dataType: "json",
+    success: function(card){
+      $(".selected_card").html('');
+      $(card).function(){
+        // debugger
+        $(".selected_card").html(
+          "<div class=card data-card-name="
+          + card.name
+          + " data-card-multiverseid="
+          + card.multiverseid
+          + ">"
+          + "<h1>Selected Card</h1>"
+          + "<img src="
+          + card.imageUrl
+          + " alt="
+          + card.name
+          + ">"
+          + "<br>"
+          // + "<a class='btn btn-primary' role='button' method='POST' href='/deck_cards'> +1 your Deck </a>  "
+          + "<button class='btn btn-primary' formmethod='POST' href='/deck_cards'> +1 your Deck </button>  "
+          + "<a class='btn btn-warning' role='button' method='POST' href='/deck_cards'> +4 your Deck </a>"
+          + "</div>");
+      })
+    },
+    error: function(){
+      alert("Something went wrong")
+    }
+  })
+}
+;
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+;
 /* ========================================================================
  * Bootstrap: affix.js v3.3.6
  * http://getbootstrap.com/javascript/#affix
