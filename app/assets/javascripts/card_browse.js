a7d69bf0002862232293e888b9c764154d7ecd4a
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-26 21:25:28 -0600

    Trying to fix things - just breaking them instead

--- app/assets/javascripts/card_browse.js
+++ app/assets/javascripts/card_browse.js
@@ -6,33 +6,38 @@ function getCard(){
   // event.preventDefault <-- probably not necessary here?
   var card_name = $(this).data("name")
   var card_multiverseid = $(this).data("multiverseid")
-  // will need a "this" for the card multiverseid
 
   $.ajax({
     url: "http://127.0.0.1:3000/api/v1/cards/" + card_multiverseid,
     // make this url unique to the card clicked and have it return internal API data
-    success: function(){
-      debugger
+    method: "GET",
+    dataType: "json",
+    success: function(card){
+      $(".selected_card").html('');
+      $(card).each(function(index, card){
+        // debugger
+        $(".selected_card").append(
+                  "<div class=card data-card-name="
+                  + card.name
+                  + " data-card-multiverseid="
+                  + card.multiverseid
+                  + ">"
+                  + "<h1>Selected Card</h1>"
+                  + "<img src="
+                  + card.imageUrl
+                  + " alt="
+                  + card.name
+                  + ">"
+                  + "<br>"
+                  // + "<a class='btn btn-primary' role='button' method='POST' href='/deck_cards'> +1 your Deck </a>  "
+                  + "<button class='btn btn-primary' formmethod='POST' href='/deck_cards'> +1 your Deck </button>  "
+                  + "<a class='btn btn-warning' role='button' method='POST' href='/deck_cards'> +4 your Deck </a>"
+                  + "</div>");
+      })
 
-      $(".selected_card").html('')
-      $(".selected-card").html(
-        "<div class=card data-card-name="
-        + card_name
-        + " data-card-multiverseid="
-        + card_multiverseid
-        + ">"
-        + "<h1>Selected Card</h1>"
-        + "<img src="
-        + card_image
-        + " alt="
-        + card_name
-        + ">"
-        + "<br>"
-        // + "<a class='btn btn-primary' role='button' method='POST' href='/deck_cards'> +1 your Deck </a>  "
-        + "<button class='btn btn-primary' formmethod='POST' href='/deck_cards'> +1 your Deck </button>  "
-        + "<a class='btn btn-warning' role='button' method='POST' href='/deck_cards'> +4 your Deck </a>"
-        + "</div>"
-      )
+    },
+    error: function(){
+      alert("Something went wrong")
     }
   })
 }
