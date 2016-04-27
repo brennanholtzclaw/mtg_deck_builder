b714f13d45f9f697e825242134b12e8741837153
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-27 00:34:59 -0600

    Refactor buttons to add cards to deck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   before_action :reset_deck
   helper_method :current_user
