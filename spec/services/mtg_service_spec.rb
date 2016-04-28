0128097e959cc372bb4ade707e1bbfd51f9b00a2
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-28 00:06:15 -0600

    test MTG service with VCR

--- spec/services/mtg_service_spec.rb
+++ spec/services/mtg_service_spec.rb
@@ -3,10 +3,42 @@ require 'rails_helper'
 RSpec.describe MtgService do
   context "#cards" do
     it "returns cards" do
-      # VCR.use_cassette()
+      @service = MtgService.new
+      VCR.use_cassette('mtg_service#cards') do
+        cards = @service.cards
+        card = cards["cards"].first
+
+        expect(card["name"]).to eq "Wicker Witch"
+      end
     end
   end
+
+  context "#sets" do
+    it "returns sets" do
+      @service = MtgService.new
+      VCR.use_cassette('mtg_service#sets') do
+        sets = @service.sets
+        set = sets.first
+
+        expect(set["code"]).to eq "DTK"
+      end
+    end
+  end
+
+  context "#get_set_cards" do
+    it "returns sets" do
+      @service = MtgService.new
+      VCR.use_cassette('mtg_service#set_cards') do
+        set = @service.get_set_cards("ORI")
+
+        expect(set["cards"].first["set"]).to eq "ORI"
+      end
+    end
+  end
+
+
 end
+
 
 # describe SunlightService do
 #   context "#legislators" do
